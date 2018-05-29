596ab44d7d7e6f2eed20d4a4f674ddd4f1439ac7
christopheleray <christophe.leray1@gmail.com>
2018-05-29 13:36:47 +0200

    countdown to 0

--- exo_13.rb
+++ exo_13.rb
@@ -3,13 +3,14 @@
 
 puts 'quelle est ton année de naissance?'
 print">"
-annee_naissance =  gets.chomp
+annee_naissance =  gets.to_i
 
 
 loop do 
 	puts "#{annee_naissance}"
-	annee_naissance = annee_naissance.to_i + 1
+	annee_naissance += 1
 	if annee_naissance == 2018
+		puts annee_naissance
 		break
 	end
 end
