ccba543c1edc0c46fa19c3bec15b1fe7e2611fa9
christopheleray <christophe.leray1@gmail.com>
2018-06-05 11:30:40 +0200

    Pyramide 2 example modification

--- exo_20.rb
+++ exo_20.rb
@@ -15,20 +15,35 @@ print ">"
 etage = gets.to_i
 
 puts etage
-puts "Voici la pyramide:"
+puts "Voici la pyramide, example1:"
 
 puts pyramide 
 
+#Example 1 affichage en "bloc"
+
 loop do 
-#	puts pyramide
 	pyramide << pyramide[ct] + "#{i}" 
 	puts pyramide[-1]
     ct += 1
-	sleep(1)
 	if ct == etage - 1
 		break
 	end
 end
 
-# J'affiche le contenu du tableau 
-#puts pyramide
+#Example 2 affichage ligne par ligne de haut en bas
+
+ct =0
+pyramide = ["#"]
+
+puts "Voici la pyramide, example2:"
+puts pyramide 
+
+loop do 
+	sleep(1)
+	pyramide << pyramide[ct] + "#{i}" 
+	puts pyramide[-1]
+    ct += 1
+	if ct == etage - 1
+		break
+	end
+end
